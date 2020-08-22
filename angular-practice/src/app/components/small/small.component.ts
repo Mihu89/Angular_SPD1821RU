dfe563de582d5d01a42d1a1a1adb34bb29567b81
mihail.coscodan <mihail.coscodan@dassolutions.eu>
2020-08-22 12:15:48 +0300

    Using Binding, NgClass, NgStyle, property binding

--- angular-practice/src/app/components/small/small.component.ts
+++ angular-practice/src/app/components/small/small.component.ts
@@ -2,12 +2,19 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-small',
-  template: '<h2>Hello from {{title}} component</h2>',
+  // template: '<h2>Hello from {{title}} component</h2>',
+  template: `<h2>Hello from {{title}} component</h2>
+            <p>Tiny details info section</p>
+            <ol><li>First section</li><li>Seccond section</li>
+            <ol>
+            <div>Made by <b>{{firstName}} {{lastName}}</b></div>`,
   styles: ['h2 {font-wheight: bold; color: violet;}']
 })
 export class SmallComponent implements OnInit {
 
   title="Small/tiny";
+  firstName: string = "John"; // display on tremplate this is interpolation, one way binding
+  lastName = 'Doe';
   constructor() { }
 
   ngOnInit(): void {
--- angular-practice/src/app/components/test/test.component.ts
+++ angular-practice/src/app/components/test/test.component.ts
@@ -7,9 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestComponent implements OnInit {
 
+  currentDay = "Saturday";
+  isDisabled = false;
+  closeLabel = "ASD";
+  cssVariable = 'red';
+  cssClass : CssClass = new CssClass();
+  status: string = 'payed';
+  size= 10;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  closeMe() {
+    console.log("Is closing");
+    if(this.cssVariable == 'red'){
+      this.cssVariable = 'blue';
+    }else{
+      this.cssVariable = 'red';
+    }
+  }
+  setColor(){
+    // console.log(this.status);
+    // var a = this.status == 'payed' ? 'red': 'blue';
+    // console.log('### ',a);
+    // return a;
+    return this.status == 'payed' ? 'red': 'blue';
+  }
+
+}
+class CssClass {
+  red: boolean = false;
+  size18: boolean = true;
+  blue: boolean = true;
 }
